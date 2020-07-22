886076a5daaf21d16b6044c386643db781fe57df
fsclaudio <fsclaudio@gmail.com>
2020-07-22 15:18:43 -0300

    Tabelas do post

---
+++ Migrations/20200722181736_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace apicampo.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tarefas",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(nullable: false),
+                    UsuarioId = table.Column<Guid>(nullable: false),
+                    nome = table.Column<string>(nullable: false),
+                    Concluida = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefas", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(nullable: false),
+                    nome = table.Column<string>(nullable: false),
+                    email = table.Column<string>(nullable: false),
+                    senha = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarefas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
---
+++ Script.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" character varying(150) NOT NULL,
+    "ProductVersion" character varying(32) NOT NULL,
+    CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId")
+);
+
+CREATE TABLE "Tarefas" (
+    id uuid NOT NULL,
+    "UsuarioId" uuid NOT NULL,
+    nome text NOT NULL,
+    "Concluida" boolean NOT NULL,
+    CONSTRAINT "PK_Tarefas" PRIMARY KEY (id)
+);
+
+CREATE TABLE "Usuarios" (
+    id uuid NOT NULL,
+    nome text NOT NULL,
+    email text NOT NULL,
+    senha text NOT NULL,
+    CONSTRAINT "PK_Usuarios" PRIMARY KEY (id)
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20200722181736_InitialCreate', '3.1.4');
+

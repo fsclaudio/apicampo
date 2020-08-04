23f7825b36b60a66a125f4254d7bcaaec2514785
fsclaudio <fsclaudio@gmail.com>
2020-08-04 10:18:46 -0300

    teste de revisa

--- Startup.cs
+++ Startup.cs
@@ -39,8 +39,7 @@ namespace apicampo
                 };
              });
            // services.AddDbContext<DataContext>(Options => Options.UseInMemoryDatabase("BDTarefas"));
-           // services.AddDbContext<DataContext>(options => options.UseNpgsql(Configuration.GetConnectionString("Hirokupost")));
-            services.AddDbContext<DataContext>(options => options.UseNpgsql(Configuration.GetConnectionString("Postgres")));
+            services.AddDbContext<DataContext>(options => options.UseNpgsql(Configuration.GetConnectionString("Hirokupost")));
             services.AddTransient<ITarefaRepository, TarefaRepository>();
             services.AddTransient<IUsuarioRepository, UsuarioRepository>();
             services.AddTransient<IVariaveisRepository, VariaveisRepository>();
